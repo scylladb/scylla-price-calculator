947f499e0990175220850b70f0788390c1dedde8
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2020-10-13 03:10:48 +0300

    merge scylla.ts with vue component

--- src/components/ScyllaCloud.vue
+++ src/components/ScyllaCloud.vue
@@ -27,8 +27,59 @@
 <script lang="ts">
 import Vue from 'vue'
 import Dropdown from './Dropdown.vue'
-import {MODE, estimatePrice} from '../scylla'
-import { CloudPricing } from '../common'
+import { CloudPricing, WorkloadSpec } from '../common'
+
+enum MODE {
+    CQL = "CQL",
+    LWT = "LWT",
+    NoLWT = "NoLWT"
+}
+
+export interface PerfModeData {
+    reads: number;
+    writes: number;
+}
+
+
+const vcpuPerf: Record<MODE, PerfModeData> = {
+    [MODE.CQL]: {
+        reads: 6250,
+        writes: 8000
+    },
+    [MODE.LWT]: {
+        writes: 1200,
+        reads: 4000
+    },
+    [MODE.NoLWT]: {
+        writes: 6000,
+        reads: 4000
+    }
+}
+
+
+const ScyllaCloudvCPUPricing: CloudPricing = {
+    onDemand: 120,
+    reserved: 77
+}
+
+const ScyllCloudStoragePervCPU = 230
+const CompactionOverhead = 2
+
+function estimatePrice(
+    workload: WorkloadSpec,
+    replicationFactor: number,
+    mode: MODE): CloudPricing {
+        console.log(mode)
+    const perf = vcpuPerf[mode]
+    const vcpus = Math.ceil(workload.reads / perf.reads + workload.writes / perf.writes)
+    const storageUnits = Math.ceil(workload.storage*replicationFactor*CompactionOverhead / ScyllCloudStoragePervCPU)
+    const vcpuUnits = Math.max(vcpus, storageUnits) 
+
+    return {
+        onDemand: vcpuUnits*ScyllaCloudvCPUPricing.onDemand,
+        reserved: vcpuUnits*ScyllaCloudvCPUPricing.reserved 
+    }
+}
 
 const modes: Record<string, MODE> = {
     "CQL": MODE.CQL,
