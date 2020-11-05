e80bcd6c7ba4e800aad8895fd122b8f39aed58f6
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2020-11-05 19:57:33 +0200

    number inputs

--- src/Calculator.vue
+++ src/Calculator.vue
@@ -3,15 +3,15 @@
         <div class="row">
             <form>
             <div class="row form-group">
-                <label class="col-sm-6 col-form-label" for="reads">Read ops/sec</label><input v-model="workload.reads" class="col-sm-3 form-control" type="text" name="reads" id="reads">
+                <label class="col-sm-6 col-form-label" for="reads">Read ops/sec</label><input v-model="workload.reads" class="col-sm-3 form-control" type="number" name="reads" id="reads">
             </div>
             <div class="row form-group">
-                <label class="col-sm-6 col-form-label" for="writes">Write ops/sec</label><input v-model="workload.writes" class="col-sm-3 form-control" type="text" name="writes" id="writes">
+                <label class="col-sm-6 col-form-label" for="writes">Write ops/sec</label><input v-model="workload.writes" class="col-sm-3 form-control" type="number" name="writes" id="writes">
             </div>
             <div class="row form-group">
-                <label class="col-sm-6 col-form-label" for="item-size">Average item size (KB)</label><input v-model="workload.itemSize" class="col-sm-3 form-control" type="text" name="items-size" id="item-size">
+                <label class="col-sm-6 col-form-label" for="item-size">Average item size (KB)</label><input v-model="workload.itemSize" class="col-sm-3 form-control" type="number" name="items-size" id="item-size">
             </div>
-            <div class="row form-group"><label class="col-sm-6 col-form-label" for="storage-size">Storage set size (GB)</label><input class="col-sm-3 form-control" type="text" v-model="workload.storage" name="storage-size" id="storage-size"></div>
+            <div class="row form-group"><label class="col-sm-6 col-form-label" for="storage-size">Storage set size (GB)</label><input class="col-sm-3 form-control" type="number" v-model="workload.storage" name="storage-size" id="storage-size"></div>
             </form>
         </div>
         <hr>
